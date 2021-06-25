5e220876a4b8a8df9299604da300d9a58c078383
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-06-25 21:12:25 +0530

    Improved Design and Enabled CORS

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const mainRoutes = require('./routes/mainRoutes');
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
- app.use(cors());
+app.use(cors({origin: 'https://youtubedl.gq'}));
 app.use(mainRoutes);
 app.listen(PORT);
 console.log('Server is live on port:'+PORT);
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
       },
       backgroundImage: theme => ({
         'youtube-logo': "url('logo.svg')",
+        'loading': "url('fetching.svg')"
        }),
        backgroundSize: {
        '20vh': '20vh',
        '30vh': '30vh',
+       '35vh': '35vh',
        '40vh': '40vh',
        '30%': '30%',
        '40%': '40%',
