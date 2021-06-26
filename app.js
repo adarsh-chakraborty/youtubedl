8b2843982c16cd77a25408b78fd3e968bdd414ff
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-06-26 14:24:21 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const mainRoutes = require('./routes/mainRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(express.static('public'));
 // app.use(cors({origin: 'https://youtubedl.gq'}));
-app.use(cors());
+
 app.use(mainRoutes);
 app.listen(PORT);
 console.log('Server is live on port:'+PORT);
